11a84e7d62dd5d22510ef9a7ec62860102ba5cac
Ben Jones <ben@bnjns.uk>
2020-06-26 18:32:06 +0100

    tweak auth helper

--- src/test/kotlin/org/backstage/Helpers.kt
+++ src/test/kotlin/org/backstage/Helpers.kt
@@ -1,19 +1,22 @@
 package org.backstage
 
+import com.nhaarman.mockitokotlin2.doAnswer
 import com.nhaarman.mockitokotlin2.doReturn
 import com.nhaarman.mockitokotlin2.mock
 import io.quarkus.oidc.runtime.OidcJwtCallerPrincipal
 import io.quarkus.security.identity.SecurityIdentity
+import java.security.Principal
 
 object AuthHelpers {
     const val DEFAULT_USER_ID = "USER_ID"
 
-    fun createMockedIdentity(userId: String = DEFAULT_USER_ID): SecurityIdentity {
-        val principle = mock<OidcJwtCallerPrincipal> {
+    fun createMockedPrincipal(userId: String = DEFAULT_USER_ID): Principal =
+        mock<OidcJwtCallerPrincipal> {
             on { subject } doReturn userId
         }
-        return mock {
-            on { principal } doReturn principle
+
+    fun createMockedIdentity(userId: String = DEFAULT_USER_ID): SecurityIdentity =
+         mock {
+            on { principal } doAnswer { createMockedPrincipal(userId) }
         }
-    }
 }
